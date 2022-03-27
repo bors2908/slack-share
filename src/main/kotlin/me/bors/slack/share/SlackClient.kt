9484264cc2a5915b7b1f6423b091e7e4b6dfa7cf
Boris_Ivanov <bors2908@gmail.com>
2022-03-27 17:18:11 +0300

    Add send methods

--- src/main/kotlin/me/bors/slack/share/SlackClient.kt
+++ src/main/kotlin/me/bors/slack/share/SlackClient.kt
@@ -1,15 +1,22 @@
 package me.bors.slack.share
 
 import com.slack.api.Slack
+import com.slack.api.methods.SlackApiTextResponse
+import com.slack.api.methods.request.chat.ChatPostMessageRequest
 import com.slack.api.methods.request.conversations.ConversationsListRequest
 import com.slack.api.methods.request.conversations.ConversationsMembersRequest
+import com.slack.api.methods.request.files.FilesUploadRequest
 import com.slack.api.methods.request.users.UsersInfoRequest
 import com.slack.api.methods.request.users.UsersListRequest
-import com.slack.api.methods.response.conversations.ConversationsListResponse
 import com.slack.api.model.Conversation
 import com.slack.api.model.ConversationType
+import com.slack.api.model.block.SectionBlock
+import com.slack.api.model.block.composition.MarkdownTextObject
+import java.io.File
+import java.io.FileNotFoundException
 import me.bors.slack.share.Utils.getProperties
 
+//TODO Add multithreading to reduce loading time.
 class SlackClient {
     private val slack = Slack.getInstance()
     private val props = getProperties("application.properties")
@@ -23,10 +30,13 @@ class SlackClient {
             UsersListRequest.builder()
                 .token(token)
                 .build()
-        ).members
+        )
+            .processErrors()
+            .members
             .associate { it.id to (it.realName ?: it.name ?: it.id) }
     }
 
+    // TODO Sort by relevance
     fun receiveChannels(): MutableList<Pair<String, String>> {
         val ims = getChannels(listOf(ConversationType.IM))
         val mpims = getChannels(listOf(ConversationType.MPIM))
@@ -37,7 +47,7 @@ class SlackClient {
         val mpimMap = mpims.map { it.id to getMultiUserGroupName(it.id) }
         val channelMap = channels.map { it.id to it.nameNormalized }
 
-        val result = mutableListOf<Pair<String,String>>()
+        val result = mutableListOf<Pair<String, String>>()
         result.addAll(imMap)
         result.addAll(mpimMap)
         result.addAll(channelMap)
@@ -56,7 +66,9 @@ class SlackClient {
                 .user(user)
                 .build()
 
-            val usersInfo = slack.methods(token).usersInfo(usersInfoRequest).user
+            val usersInfo = slack.methods(token).usersInfo(usersInfoRequest)
+                .processErrors()
+                .user
 
             usersInfo.realName ?: usersInfo.name ?: usersInfo.id
         }
@@ -69,7 +81,9 @@ class SlackClient {
             .build()
 
         //TODO Add pagination support
-        val members = slack.methods(token).conversationsMembers(request).members
+        val members = slack.methods(token).conversationsMembers(request)
+            .processErrors()
+            .members
 
         members.removeAt(members.size - 1)
 
@@ -87,12 +101,99 @@ class SlackClient {
             .build()
 
         //TODO Add pagination support
-        val conversationsList: ConversationsListResponse = slack.methods(token).conversationsList(request)
+        return slack.methods(token).conversationsList(request)
+            .processErrors()
+            .channels
+    }
 
-        return conversationsList.channels
+    fun sendMessage(id: String, text: String, quoteCode: Boolean = false) {
+        // TODO make it quote
+        val builder = ChatPostMessageRequest.builder()
+            .token(token)
+            .channel(id)
+
+        if (quoteCode) {
+            builder
+                .mrkdwn(true)
+                .blocks(
+                    listOf(
+                        SectionBlock.builder()
+                            .text(
+                                MarkdownTextObject.builder()
+                                    .text(processMarkdownAndQuote(text))
+                                    .build()
+                            )
+                            .build()
+                    )
+                )
+        } else {
+            builder
+                .mrkdwn(false)
+                .text(text)
+        }
+
+        val request = builder.build()
+
+        //TODO Remove
+        println("Sending message to channel $id")
+
+        val response = slack.methods(token).chatPostMessage(request).processErrors()
+    }
+
+    private fun processMarkdownAndQuote(text: String): String {
+        val result = text
+            .replace("<", "&lt;")
+            .replace(">", "&rt;")
+            .replace("&", "&amp;")
+
+        return "```$result```"
+    }
+
+    fun sendFile(id: String, files: List<File>, text: String) {
+        var tagged = false
+
+        // TODO Switch to File blocks message markdown
+        for (file: File in files) {
+            if (!file.exists()) {
+                throw FileNotFoundException("File not found [$file].")
+            }
+
+            //TODO make it use path instead of loading bytes
+            val builder = FilesUploadRequest.builder()
+                .token(token)
+                .channels(listOf(id))
+                .fileData(file.readBytes())
+                .filename(file.name)
+
+            if (text.isNotEmpty() && !tagged) {
+                builder.initialComment(text)
+
+                tagged = true
+            }
+
+            val request = builder.build()
+
+            val response = slack.methods(token).filesUpload(request).processErrors()
+        }
     }
 }
 
-fun main() {
-    SlackClient()
+fun <T : SlackApiTextResponse> T.processErrors(): T {
+    if (this.warning != null) {
+        //TODO Uncomment after logging stuff is resolved
+        //log.warn { "Warining received from Slack: ${this.warning}" }
+    }
+
+    if (this.error != null) {
+        val needed = if (this.needed != null) "Needed: ${this.needed}" else ""
+        val provided = if (this.provided != null) "Provided: ${this.provided}" else ""
+
+
+        throw RuntimeException(
+            "Error occurred, during Slack request execution: " +
+                    "${this.error} ${System.lineSeparator()} $needed ${System.lineSeparator()} $provided"
+        )
+    }
+
+    return this
 }
