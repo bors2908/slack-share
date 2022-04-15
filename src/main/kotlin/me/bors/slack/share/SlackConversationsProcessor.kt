cf39c16fe77ccb375b71a6d0181a3504ab029f3f
Boris_Ivanov <bors2908@gmail.com>
2022-04-15 19:31:07 +0300

    Migrate conversations requests to Kotlin Coroutines

--- src/main/kotlin/me/bors/slack/share/SlackConversationsProcessor.kt
+++ src/main/kotlin/me/bors/slack/share/SlackConversationsProcessor.kt
@@ -1,50 +1,50 @@
 package me.bors.slack.share
 
 import com.slack.api.model.ConversationType
-import java.util.concurrent.CountDownLatch
 import java.util.concurrent.Executors
 import java.util.concurrent.LinkedBlockingQueue
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 open class SlackConversationsProcessor(protected val slackClient: SlackClient) {
     fun getConversations(): List<SlackConversation> {
         /* Multi-User conversations require a lot of requests to receive members and form a readable conversation name
            Concurrent execution helps to reduce execution time up to 4x
          */
-        val pool = Executors.newFixedThreadPool(
-            (Runtime.getRuntime().availableProcessors() / 2).coerceAtLeast(2)
-        )
-
         val result = LinkedBlockingQueue<SlackConversation>()
 
         val multiChannels = slackClient.getChannels(listOf(ConversationType.MPIM))
 
-        val latch = CountDownLatch(multiChannels.size)
+        val dispatcher = Executors.newCachedThreadPool().asCoroutineDispatcher()
 
-        multiChannels.forEach {
-            pool.submit {
-                result.add(SlackConversation(it.id,
-                    slackClient.getMultiUserGroupName(it.id),
-                    it.priority ?: 0.0))
+        runBlocking {
+            multiChannels.forEach {
+                launch(dispatcher) {
+                    result.add(
+                        SlackConversation(
+                            it.id,
+                            slackClient.getMultiUserGroupName(it.id),
+                            it.priority ?: 0.0
+                        )
+                    )
+                }
+            }
 
-                latch.countDown()
+            launch {
+                result.addAll(
+                    slackClient.getChannels(listOf(ConversationType.IM))
+                        .map { SlackConversation(it.id, slackClient.getUserName(it.user), it.priority ?: 0.0) }
+                )
+            }
+
+            launch {
+                result.addAll(
+                    slackClient.getChannels(listOf(ConversationType.PRIVATE_CHANNEL, ConversationType.PUBLIC_CHANNEL))
+                        .map { SlackConversation(it.id, it.nameNormalized, it.priority ?: 0.0) }
+                )
             }
         }
-
-        result.addAll(
-            slackClient.getChannels(listOf(ConversationType.IM))
-                .map { SlackConversation(it.id, slackClient.getUserName(it.user), it.priority ?: 0.0) }
-        )
-
-        result.addAll(
-            slackClient.getChannels(listOf(ConversationType.PRIVATE_CHANNEL, ConversationType.PUBLIC_CHANNEL))
-                .map { SlackConversation(it.id, it.nameNormalized, it.priority ?: 0.0) }
-        )
-
-        // Await moved lower to allow main thread to process more streamlined IMs and channels while MLIMs are
-        // processed in the pool.
-        latch.await()
-
-        pool.shutdown()
 
         return result.sortedByDescending { it.priority }
     }
